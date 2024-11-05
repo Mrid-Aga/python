d7926d785e0d595143345e075bb4dc9096f5d96c
Mrid-Aga <151604696+Mrid-Aga@users.noreply.github.com>
2024-11-05 14:02:45 +0800

    Add files via upload

---
+++ assignments/lamma.py
@@ -0,0 +1,19 @@
+start = 0
+while True:
+    try:
+        start = int(input("How many lammas to start off with? "))
+        start_permanant = start
+    except ValueError:
+        print("please put in a number bigger than 9")
+    if start <9:
+        print("please put in a number bigger than 9")
+    else:
+        break
+end = int(input("What population do you want it to stop at?"))
+
+while start < end:
+    start+=(start/3-start/4)
+    print("Population"+start)
+
+print("Start Size: "+str(start_permanant))
+print("Start Size: "+str(start_permanant))
