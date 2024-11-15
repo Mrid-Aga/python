4505cc71cbbbf50b99ff67289f85ed3dde4e36c5
Mrid-Aga <151604696+Mrid-Aga@users.noreply.github.com>
2024-11-15 13:13:34 +0000

    prime loops assignment

--- assignments/prime_loops.py
+++ assignments/prime_loops.py
@@ -1,5 +1,16 @@
+import itertools
 start = int(input("Where should it start looking for primes? "))
 end = int(input("Where should it stop looking? "))
+num_list=list(range(2,end+1))
 
-num_list=list(range(start,end+1))
+combinations = (list(itertools.product(list(range(2,end)), num_list)))
+multiplications = []
 
+for tuples in combinations:
+    mult = tuples[0]*tuples[1]
+    multiplications.append(mult)
+
+for i in multiplications:
+    if i in num_list:
+        num_list.remove(i)
+print(num_list)
